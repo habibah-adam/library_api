7a992913b5b4cae32c48c14f39e89200657b0124
Habibah Adam <hebibe2s@gmail.com>
2021-01-04 19:22:16 +1100

    foreinkey update

--- models/Page.py
+++ models/Page.py
@@ -4,9 +4,9 @@ class Page(db.Model):
     __tablename__ = "pages"
     
     id = db.Column(db.Integer, primary_key=True)
-    page_number = db.Column(db.Integer)
-    page_content = db.Column(db.String())
-    book_id = db.Column(db.Integer, db.ForeignKey("books.id"), nullable=False)
+    page_number = db.Column(db.Integer, nullable=False)
+    page_content = db.Column(db.Text())
+    book_id = db.Column(db.Integer, db.ForeignKey("book.id"), nullable=False)
 
     def __repr__(self):
-        return f"<Page {self.page_number}, Book: {self.id}>"
+        return f"<Page {self.page_number}, Book: {self.book_id}>"
